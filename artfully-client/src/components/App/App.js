20059bb3d204b94c6fe4002b24e8ef16fd04682f
Luke Osgood <lukemosgood@gmail.com>
2019-11-28 23:20:25 -0500

    just getting started

--- artfully-client/src/components/App/App.js
+++ artfully-client/src/components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import logo from '../../assets/images/logo.png';
 
 function App() {
   return (
     <div className="App">
-      
+      <img src={logo} alt=""/>
     </div>
   );
 }
